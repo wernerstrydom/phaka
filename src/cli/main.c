54d0ef88d91d00fe8e7e955074feb49b407450eb
Werner Strydom <bloudraak@users.noreply.github.com>
2023-02-26 13:02:31 -0800

    Bootstrapping
    Since the application provides capabilities to improve the release management process, we'll install it and then use it to help build itself.

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-	printf("Hello, World!\n");
-	return 0;
+#if defined(__linux__)
+#define PHAKA_SYSTEM "linux"
+#elif defined(__APPLE__)
+#define PHAKA_SYSTEM "darwin"
+#elif defined(_WIN32)
+#define PHAKA_SYSTEM "windows"
+#else
+#error "Unsupported system"
+#endif
+
+#if defined(__x86_64__)
+#define PHAKA_ARCH "amd64"
+#elif defined(__i386__)
+#define PHAKA_ARCH "i386"
+#elif defined(__arm__)
+#define PHAKA_ARCH "arm"
+#else
+#error "Unsupported architecture"
+#endif
+
+int main(int argc, char *argv[]) {
+  switch (argc) {
+  case 2:
+    if (strcmp(argv[1], "arch") == 0) {
+      printf("%s\n", PHAKA_ARCH);
+      return 0;
+    }
+
+    if (strcmp(argv[1], "system") == 0) {
+      printf("%s\n", PHAKA_SYSTEM);
+      return 0;
+    }
+
+    if (strcmp(argv[1], "--version") == 0) {
+      printf("phaka 0.1.0\n");
+      return 0;
+    }
+  }
+  return 1;
 }
